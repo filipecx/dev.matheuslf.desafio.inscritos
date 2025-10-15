0b4727f3bab2d3fd18fc9fcc2ec529ee261fdb17
Filipe C <filipecavalcantiximenes@gmail.com>
2025-10-15 10:18:44 -0300

    :sparkles: added task class

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/domain/model/Task.java
@@ -0,0 +1,75 @@
+package dev.matheuslf.desafio.inscritos.domain.model;
+
+import dev.matheuslf.desafio.inscritos.domain.Exceptions.DomainException;
+import dev.matheuslf.desafio.inscritos.domain.model.enums.Priority;
+import dev.matheuslf.desafio.inscritos.domain.model.enums.Status;
+
+import java.time.LocalDate;
+
+public class Task {
+    private Long id;
+    private String title;
+    private String description;
+    private Status status;
+    private Priority priority;
+    private LocalDate dueDate;
+
+    public Task(String title, String description, Status status, Priority priority, LocalDate dueDate) {
+        this.validateTitle(title);
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public Priority getPriority() {
+        return this.priority;
+    }
+
+    public LocalDate getDueDate() {
+        return this.dueDate;
+    }
+
+    public void changeTitle(String newTitle) {
+        validateTitle(newTitle);
+        this.title = newTitle;
+    }
+
+    public void changeDescription(String newDescription) {
+        this.description = newDescription;
+    }
+
+    public void changeStatus(Status newStatus) {
+        this.status = newStatus;
+    }
+
+    public void changePriority(Priority newPriority) {
+        this.priority = newPriority;
+    }
+
+    private void validateTitle(String title) {
+        if (title == null) {
+            throw new DomainException("Você deve inserir um título para criar uma tarefa");
+        }
+        if (title.length() < 5) {
+            throw new DomainException("O título da tarefa deve ter pelo menos 5 caracteres");
+        }
+        if (title.length() > 150) {
+            throw new DomainException("O título da tarefa não pode passar de 150 caracteres");
+        }
+    }
+
+}
