6c26ed647b45026807b6ce8b5031f8e7d4506cc4
Filipe C <filipecavalcantiximenes@gmail.com>
2025-10-15 08:53:59 -0300

    :sparkles: added project class

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/domain/model/Project.java
@@ -0,0 +1,89 @@
+package dev.matheuslf.desafio.inscritos.domain.model;
+
+import dev.matheuslf.desafio.inscritos.domain.Exceptions.DomainException;
+
+import java.time.LocalDate;
+
+public class Project {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    private Project (Builder builder) {
+        this.validateDate(builder.startDate, builder.endDate);
+        this.validateName(builder.name);
+        this.name = builder.name;
+        this.description = builder.description;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+    }
+
+    public static class Builder {
+        private String name;
+        private String description = null;
+        private LocalDate startDate;
+        private LocalDate endDate = null;
+
+        public Builder (String name, LocalDate startDate) {
+            this.name = name;
+            this.startDate = startDate;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder endDate(LocalDate endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public Project build() {
+            return new Project(this);
+        }
+    }
+
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public LocalDate getStartDate() {
+        return this.startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return this.endDate;
+    }
+
+    private void validateDate(LocalDate startDate, LocalDate endDate) {
+        if (startDate == null) {
+            throw new DomainException("A data de início é obrigatória. ");
+        }
+        if (endDate == null) {
+            return;
+        }
+        if (startDate.isAfter(endDate)) {
+            throw new DomainException("A data de início não pode ser depois da data de término. ");
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.length() < 3) {
+            throw new DomainException("O nome do projeto não pode ter menos de três letras. ");
+        }
+    }
+
+}
