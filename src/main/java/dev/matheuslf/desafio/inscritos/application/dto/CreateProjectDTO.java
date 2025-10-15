8af64f58229cc6bba81ea5648b01ce7ef17d2e8e
Filipe C <filipecavalcantiximenes@gmail.com>
2025-10-15 19:15:05 -0300

    :sparkles: added dtos with bean validation

--- src/main/java/dev/matheuslf/desafio/inscritos/application/dto/CreateProjectDTO.java
+++ src/main/java/dev/matheuslf/desafio/inscritos/application/dto/CreateProjectDTO.java
@@ -1,8 +1,13 @@
 package dev.matheuslf.desafio.inscritos.application.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public record CreateProjectDTO(
+        @NotBlank(message = "O projeto deve ter um nome")
+        @Size(min = 3, max = 100, message = "O nome do projeto deve ter entre {min} e {max} caracteres")
         String name,
         String description,
         LocalDate startDate,
